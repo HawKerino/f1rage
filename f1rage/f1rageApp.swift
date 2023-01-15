f7743ef74400d15a2d43eda3748bc6885ba9a235
Dušan Papulák <papulakdusan@gmail.com>
2023-01-16 00:02:37 +0100

    Initial Commit

---
+++ f1rage/f1rageApp.swift
@@ -0,0 +1,17 @@
+//
+//  f1rageApp.swift
+//  f1rage
+//
+//  Created by Dušky Papulák on 16/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct f1rageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
